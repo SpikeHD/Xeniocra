319455e9427a3d74ea57b968fbf490efa51f3659
SpikeHD <spikegdofficial@gmail.com>
2022-11-25 22:15:26 -0800

    full options list

--- src/components/SettingsList.tsx
+++ src/components/SettingsList.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "preact/hooks"
+import { config } from "process"
 import { setCliOption } from "../util/cliConfig"
 import { setConfigOption, getConfig, getDefaultConfig } from "../util/config"
 import { FileSelect } from "./common/FileSelect"
@@ -47,7 +48,7 @@ export function SettingsList() {
         <select onChange={(evt) => {
           // @ts-expect-error Too lazy to fix event types
           setCliOption('gpu_backend', evt.target.value)
-        }}>
+        }} value={settings.cli_options.gpu_backend}>
           <option value="any">Any (reccommended)</option>
           <option value="d3d12">Direct3D 12</option>
           <option value="vulkan">Vulkan (not reccommended)</option>
@@ -55,7 +56,23 @@ export function SettingsList() {
         </select>
       </div>
       <div class="settings_section">
-
+        <span>Other Options</span>
+        <div class="settings_checkboxes">
+          <div>
+            <label for="vsync">Vsync</label>
+            <input type="checkbox" id="vsync" name="vsync" onChange={(evt) => {
+              // @ts-expect-error Too lazy to fix event types
+              setCliOption('vsync', evt.target.checked)
+            }} checked={settings.cli_options.vsync} />
+          </div>
+          <div>
+            <label for="protect_zero">Protect Zero</label>
+            <input type="checkbox" id="protect_zero" name="protect_zero" onChange={(evt) => {
+              // @ts-expect-error Too lazy to fix event types
+              setCliOption('protect_zero', evt.target.checked)
+            }} checked={settings.cli_options.protect_zero} />
+          </div>
+        </div>
       </div>
     </>
   )
